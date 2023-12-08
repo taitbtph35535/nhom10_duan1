36694f1531e14fbc6054409642d38e1dd5f604a8
taitbtph35535 <taitbtph35535@fpt.edu.vn>
2023-12-08 07:42:27 +0700

    update:model-pay

--- model/pay.php
+++ model/pay.php
@@ -5,4 +5,10 @@ function load_sanpham_cart_of_one(){
     return pdo_query($sql);
 }
 
+
+function insert_bill($user,$allBooks,$book_quantity,$book_price,$address,$payment_method,$phone,$total){
+    $sql="INSERT INTO `bill`(`id`, `customer_id`, `books`, `book_quantity`, `book_price`,  `order_status`, `shipping_address`, `payment_method`, `phone`,`total`) 
+    VALUES ('','$user','$allBooks','$book_quantity','$book_price','Chờ xác nhận','$address','$payment_method','$phone','$total')";
+    return pdo_query($sql);
+}
 ?>
